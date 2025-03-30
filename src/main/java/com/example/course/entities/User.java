c4cf4969c0f972e30064aea84ad9d9da184377b1
Frederico Carlos <frederico_carlos@hotmail.com>
2025-03-30 20:08:42 -0230

    Order class created

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -25,6 +28,9 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -84,6 +90,11 @@ public class User implements Serializable{
 	public String getPassword() {
 		return password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
+	
 
 
 	public void setPassword(String password) {
@@ -108,7 +119,9 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
+
+
 	
 	
 }
