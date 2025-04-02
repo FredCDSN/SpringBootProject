4db0925173213fb5589a8fdb93abbaea045edd8c
Frederico Carlos <frederico_carlos@hotmail.com>
2025-04-02 11:07:06 -0230

    OrderStatus ENUM created

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.example.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,6 +27,10 @@ public class Order implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
+	
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id") //foreign key
 	private User client;
@@ -35,10 +41,11 @@ public class Order implements Serializable {
 	}
 
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+	    setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -60,6 +67,18 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueof(orderStatus);
+	}
+
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 
